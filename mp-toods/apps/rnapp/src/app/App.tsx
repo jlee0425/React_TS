2eb840d7dde89f61aac61989c30732bac7f3f61e
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-08-23 00:56:24 +0900

    react native app

---
+++ mp-toods/apps/rnapp/src/app/App.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { useTodos } from '@mp-toods/data-access';
+import { useCallback } from 'react';
+
+const App = () => {
+  const { todos, getTodos } = useTodos();
+
+  const onRefresh = useCallback(() => getTodos(), [getTodos]);
+
+  return (
+    <>
+      <StatusBar barStyle='dark-content' />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior='automatic'
+          style={styles.scrollView}
+        >
+          {todos.map(todo => (
+            <View key={todo.id}>
+              <Text style={{ fontSize: 36, padding: 5 }}>{todo.text}</Text>
+              <TouchableOpacity onPress={onRefresh}>Refresh</TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter
+  },
+  header: {
+    backgroundColor: '#143055',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 24
+  },
+  logo: {
+    width: 200,
+    height: 180,
+    resizeMode: 'contain'
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.lighter
+  },
+  body: {
+    backgroundColor: Colors.white
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark
+  },
+  highlight: {
+    fontWeight: '700'
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right'
+  },
+  link: {
+    color: '#45bc98'
+  }
+});
+
+export default App;
